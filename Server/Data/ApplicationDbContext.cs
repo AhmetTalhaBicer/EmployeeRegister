9b2ad2bd5d6ca6eb18c7d9dc560ee24b028061f1
Ahmet Talha Biçer <bicerahmettalha@gmail.com>
2024-07-11 13:52:34 +0300

    setup-2

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
