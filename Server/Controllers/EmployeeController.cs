1b2c5ef336282d87e70f2f6acf32cba2faa86b71
Ahmet Talha Biçer <bicerahmettalha@gmail.com>
2024-07-13 14:04:17 +0300

    finish

--- Server/Controllers/EmployeeController.cs
+++ Server/Controllers/EmployeeController.cs
@@ -2,156 +2,150 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ImageApp.Data;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using ImageApp.Models;
+using ImageApp.Data;
 
-namespace ImageApp.Controllers
+namespace EmployeeRegisterAPI.Controllers
 {
-    public class EmployeeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public EmployeeController(ApplicationDbContext context)
+        public EmployeeController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            this._hostEnvironment = hostEnvironment;
         }
 
-        // GET: Employee
-        public async Task<IActionResult> Index()
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeModel>>> GetEmployees()
         {
-            return View(await _context.Employees.ToListAsync());
+            return await _context.Employees
+                .Select(x => new EmployeeModel()
+                {
+                    EmployeeID = x.EmployeeID,
+                    EmployeeName = x.EmployeeName,
+                    Occupation = x.Occupation,
+                    ImageName = x.ImageName,
+                    ImageSrc = String.Format("{0}://{1}{2}/Images/{3}", Request.Scheme, Request.Host, Request.PathBase, x.ImageName)
+                })
+                .ToListAsync();
         }
 
-        // GET: Employee/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeModel>> GetEmployeeModel(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employeeModel = await _context.Employees
-                .FirstOrDefaultAsync(m => m.EmployeeID == id);
-            if (employeeModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(employeeModel);
-        }
-
-        // GET: Employee/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Employee/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EmployeeID,EmployeeName,Occupation,ImageName")] EmployeeModel employeeModel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employeeModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employeeModel);
-        }
-
-        // GET: Employee/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var employeeModel = await _context.Employees.FindAsync(id);
+
             if (employeeModel == null)
             {
                 return NotFound();
             }
-            return View(employeeModel);
+
+            return employeeModel;
         }
 
-        // POST: Employee/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EmployeeID,EmployeeName,Occupation,ImageName")] EmployeeModel employeeModel)
+        // PUT: api/Employee/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployeeModel(int id, [FromForm] EmployeeModel employeeModel)
         {
             if (id != employeeModel.EmployeeID)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            if (employeeModel.ImageFile != null)
             {
-                try
-                {
-                    _context.Update(employeeModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeeModelExists(employeeModel.EmployeeID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                DeleteImage(employeeModel.ImageName);
+                employeeModel.ImageName = await SaveImage(employeeModel.ImageFile);
             }
-            return View(employeeModel);
+
+            _context.Entry(employeeModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Employee/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // POST: api/Employee
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<EmployeeModel>> PostEmployeeModel([FromForm] EmployeeModel employeeModel)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            employeeModel.ImageName = await SaveImage(employeeModel.ImageFile);
+            _context.Employees.Add(employeeModel);
+            await _context.SaveChangesAsync();
 
-            var employeeModel = await _context.Employees
-                .FirstOrDefaultAsync(m => m.EmployeeID == id);
+            return StatusCode(201);
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<EmployeeModel>> DeleteEmployeeModel(int id)
+        {
+            var employeeModel = await _context.Employees.FindAsync(id);
             if (employeeModel == null)
             {
                 return NotFound();
             }
-
-            return View(employeeModel);
-        }
-
-        // POST: Employee/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var employeeModel = await _context.Employees.FindAsync(id);
-            if (employeeModel != null)
-            {
-                _context.Employees.Remove(employeeModel);
-            }
-
+            DeleteImage(employeeModel.ImageName);
+            _context.Employees.Remove(employeeModel);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return employeeModel;
         }
 
         private bool EmployeeModelExists(int id)
         {
             return _context.Employees.Any(e => e.EmployeeID == id);
+        }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }
+
+        [NonAction]
+        public void DeleteImage(string imageName)
+        {
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
         }
     }
 }
